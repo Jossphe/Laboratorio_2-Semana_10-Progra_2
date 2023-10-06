ee17dfa8372bd5f6bc99a2a60723696cbb9aaa95
joser <joser@MENDOZA>
2023-10-05 18:36:18 -0600

    Agregar archivos de proyecto.

---
+++ Module1.vb
@@ -0,0 +1,85 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("UNIVERSIDAD DOCTOR ANDRRES BELLO, REGIONAL SOSNSONATE")
+        Console.WriteLine("LABORATORIO 2, PROGRAMACION 2.")
+        Console.WriteLine("SEMANA 10.")
+        Console.WriteLine()
+        Console.WriteLine("DESARROLLADORES: ")
+        Console.WriteLine("DAMARYS ELENA OSORIO GUTIERREZ.")
+        Console.WriteLine("JOSE RIGOBERTO MENDOZA RIVAS.")
+
+
+        'ejercicio1()
+        ejercicio2()
+
+        Console.Read()
+
+    End Sub
+
+    Sub ejercicio1()
+
+        Console.WriteLine()
+        Console.WriteLine("EJERCICIO 1")
+
+        Dim CostoProducto As Single
+        Dim DineroEntregado As Single
+
+        Console.WriteLine("Ingrese el costo del artículo a adquirir: $00.00")
+        CostoProducto = Convert.ToSingle(Console.ReadLine())
+
+        Console.WriteLine("Ingrese la cantidad de dinero entregada por el cliente: $00.00")
+        DineroEntregado = Convert.ToSingle(Console.ReadLine())
+
+        If DineroEntregado > CostoProducto Then
+            Dim cambio As Single = DineroEntregado - CostoProducto
+            Console.WriteLine("Cambio a entregar: $" & cambio.ToString("00.00"))
+            Console.WriteLine("¡GRACIAS POR SU COMPRA!.")
+        ElseIf DineroEntregado = CostoProducto Then
+            Console.WriteLine("¡GRACIAS POR SU COMPRA!.")
+        Else
+            Dim falta_pagar As Single = CostoProducto - DineroEntregado
+            Console.WriteLine("Falta pagar: $" & falta_pagar.ToString("00.00"))
+            If falta_pagar > 0 Then
+                Console.WriteLine("Lo sentimos su transaccion no fue realizada, por favor intente nuevamente o Vuelva pronto por el producto deseado.")
+            Else
+                Console.WriteLine("¡GRACIAS POR SU COMPRA!.")
+            End If
+        End If
+
+    End Sub
+
+    Sub ejercicio2()
+
+        Console.WriteLine()
+        Console.WriteLine("EJERCICIO 2")
+
+        Dim monto As Double
+        Dim descuento As Double
+        Dim montoFinal As Double
+
+        Console.Write("Ingrese el monto de la compra: ")
+        Console.WriteLine()
+        monto = Console.ReadLine()
+
+        While monto <> 0
+            If monto < 0 Then
+                Console.WriteLine("El monto no puede ser negativo.")
+            ElseIf monto > 1000 Then
+                descuento = monto * 0.1
+                montoFinal = monto - descuento
+                Console.WriteLine("El monto final con descuento del 10% es: " & montoFinal)
+            ElseIf monto > 500 Then
+                descuento = monto * 0.05
+                montoFinal = monto - descuento
+                Console.WriteLine("El monto final con descuento del 5% es: " & montoFinal)
+            Else
+                Console.WriteLine("El monto final sin descuento es: " & monto)
+            End If
+
+            monto = Console.ReadLine()
+        End While
+    End Sub
+
+End Module
+
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Laboratorio_2-Semana_10-Progra_2")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Laboratorio_2-Semana_10-Progra_2")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("e3cb5f59-9af4-4d32-a0f3-d007274a5d59")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
